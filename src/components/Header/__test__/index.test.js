618d31c98366bd4486db1c2d8c82a03c52fd53d0
adele tafazoli <adeletfz@gmail.com>
2020-02-28 18:19:22 +0330

    Add test to header

---
+++ src/components/Header/__test__/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from '../';
+import ShallowRenderer from 'react-test-renderer/shallow';
+
+test('smoke testing Header component', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Header />, div);
+});
+
+// Snapshot testing Header component
+test('Shallow rendering Header component', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Header />);
+  //   console.log(renderedOutput);
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
--- src/components/HomePageComponents/schedual/BuildTable/index.js
+++ src/components/HomePageComponents/schedual/BuildTable/index.js
@@ -3,22 +3,37 @@ import Table from './table';
 import './style/index.css';
 
 const BuildTable = props => {
-  let transformedclasses = Object.keys(props.classes)
-    .map(clas => {
-      return [...Array(props.classes[clas])].map((_, i) => {
-        return <Table key={clas + i} type={clas} />;
-      });
-    })
-    .reduce((arr, el) => {
-      return arr.concat(el);
-    }, []);
-  if (transformedclasses.length === 0) {
-    transformedclasses = <p>Please start adding your favorite classes</p>;
-  }
+  console.log('props.classes', props.classes);
+  // let transformedclasses = Object.keys(props.classes)
+  //   .map(clas => {
+  //     return [...Array(props.classes[clas])].map((_, i) => {
+  //       return <Table key={clas + i} type={clas} />;
+  //     });
+  //   })
+  //   .reduce((arr, el) => {
+  //     return arr.concat(el);
+  //   }, []);
+  // if (transformedclasses.length === 0) {
+  //   transformedclasses = <p>Please start adding your favorite classes</p>;
+  // }
+  const tableData = [];
+  Object.keys(props.classes).forEach(singleClassName => {
+    tableData.push({
+      type: singleClassName,
+      count: props.classes[singleClassName]
+    });
+  });
+
+  console.log('tableData', tableData);
   return (
     <div className='doS'>
       <Table type='nothing' />
-      {transformedclasses}
+      {/* {transformedclasses} */}
+      {tableData.map(singleClass =>
+        Array(singleClass.count).map((takenClass, i) => (
+          <Table key={takenClass.type + i} type={takenClass.type} />
+        ))
+      )}
     </div>
   );
 };
